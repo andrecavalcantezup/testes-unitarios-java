1fb1bab713c650258a962b4a64fa79efa51436a6
andrecavalcantezup <andre.cavalcante@zup.com.br>
2021-09-03 14:38:16 -0300

    Assertivas

---
+++ src/test/java/AssertTest.java
@@ -0,0 +1,44 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Usuario;
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		Assert.assertEquals("Erro comparação", 1, 2);
+		Assert.assertEquals(0.51, 0.51, 0.01);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i, i2.intValue());
+		
+		Assert.assertEquals("bola", "bola");
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		Assert.assertTrue("bola".startsWith("bo"));
+		Assert.assertNotEquals("bola", "casa");
+		
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = null;
+		
+		Assert.assertEquals(u1, u1);
+		Assert.assertSame(u2, u2);
+		Assert.assertNotSame(u2, u3);
+		Assert.assertTrue(u3 == null);
+		Assert.assertNull(u3);
+		Assert.assertNotNull(u2);
+		
+		
+		
+	}
+
+}
